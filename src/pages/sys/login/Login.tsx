4fdbc1095107a1dc67fc4a79dc4dd2c837c876fe
minijude <914753389@qq.com>
2024-10-13 23:01:36 +0800

    add LoginStateProvider

--- src/pages/sys/login/Login.tsx
+++ src/pages/sys/login/Login.tsx
@@ -6,6 +6,9 @@ import DashboardImg from "@/assets/images/background/dashboard.png";
 import Overlay2 from "@/assets/images/background/overlay_2.jpg";
 import { useThemeToken } from "@/theme/hooks";
 
+import { LoginStateProvider } from "./providers/LoginStateProvider";
+import LoginForm from './LoginForm'
+
 function Login() {
   const { t } = useTranslation();
   const { colorBgElevated } = useThemeToken();
@@ -24,6 +27,11 @@ function Login() {
         <div className="text-3xl font-bold leading-normal lg:text-4xl xl:text-5xl">Slash Admin</div>
         <img className="max-w-[480px] xl:max-w-[560px]" src={DashboardImg} alt="" />
         <Typography.Text className="flex flex-row gap-[16px] text-2xl">{t("sys.login.signInSecondTitle")}</Typography.Text>
+      </div>
+      <div className="m-auto flex !h-screen w-full max-w-[480px] flex-col justify-center px-[16px] lg:px-[64px]">
+        <LoginStateProvider>
+          <LoginForm></LoginForm>
+        </LoginStateProvider>
       </div>
     </Layout>
   );
---
+++ src/pages/sys/login/LoginForm.tsx
@@ -0,0 +1,10 @@
+import { useLoginStateContext } from "./providers/LoginStateProvider"
+
+
+function LoginForm() {
+  const context = useLoginStateContext()
+  debugger
+  return <div>LoginForm</div>
+}
+
+export default LoginForm
