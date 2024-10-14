9b41ae18195430ce7ea2bc8470f515f40be92b1a
minijude <914753389@qq.com>
2024-10-14 21:23:12 +0800

    集成状态管理器zustand、http库axios、异步数据状态管理@tanstack/react-query、@tanstack/react-query-devtools

--- src/main.tsx
+++ src/main.tsx
@@ -1,14 +1,34 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+// react-query
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
 
 // i18n
-import './locales/i18n.ts'
+import "./locales/i18n.ts";
 // tailwind css
-import './theme/index.css'
+import "./theme/index.css";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
+// 创建一个 client
+const queryClient = new QueryClient({
+  defaultOptions: {
+    queries: {
+      retry: 3, // 失败重试次数
+      gcTime: 300_000, // 缓存有效期 5m
+      staleTime: 10_1000, // 数据变得 "陈旧"（stale）的时间 10s
+      refetchOnWindowFocus: false, // 禁止窗口聚焦时重新获取数据
+      refetchOnReconnect: false, // 禁止重新连接时重新获取数据
+      refetchOnMount: false, // 禁止组件挂载时重新获取数据
+    },
+  },
+});
+
+const root = ReactDOM.createRoot(document.getElementById("root") as HTMLElement);
+
+root.render(
+  <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools initialIsOpen={false} />
     <App />
-  </StrictMode>,
-)
+  </QueryClientProvider>
+);
