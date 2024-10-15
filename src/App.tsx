e17cbd03cc37303ceef88326004a098142c073d8
minijude <914753389@qq.com>
2024-10-15 14:06:05 +0800

    集成head标签管理包 react-helmet-async

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
+import { App as AntdApp } from "antd";
+import { Helmet, HelmetProvider } from "react-helmet-async";
+
+import Logo from "@/assets/images/logo.png";
 import Router from "@/router";
+import AntdConfig from "@/theme/antd";
 
 function App() {
-  return <Router />;
+  return (
+    <HelmetProvider>
+      <AntdConfig>
+        <AntdApp>
+          <Helmet>
+            <title>Slash Admin</title>
+            <link rel="icon" href={Logo} />
+          </Helmet>
+          <Router />
+        </AntdApp>
+      </AntdConfig>
+    </HelmetProvider>
+  );
 }
 
 export default App;
