927643298ff2df01efc8df291c586878d07df25b
minijude <914753389@qq.com>
2024-10-23 19:10:16 +0800

    版本调整

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,7 @@
 import path from "path";
 
 import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { createSvgIconsPlugin } from "vite-plugin-svg-icons";
 import tsconfigPaths from "vite-tsconfig-paths";
 
